0b18d7c11e23c6d65169a4993ca9cce42081b133
peteinthelight <97072098+peteinthelight@users.noreply.github.com>
2025-12-06 17:25:10 -0600

    Update recordings.js

--- netlify/functions/recordings.js
+++ netlify/functions/recordings.js
@@ -104,6 +104,7 @@ exports.handler = async () => {
           h1 {
             margin: 0 0 0.5rem 0;
             font-size: 1.8rem;
+            color: white; /* match your inspector tweak */
           }
           .subtitle {
             margin-bottom: 1.5rem;
